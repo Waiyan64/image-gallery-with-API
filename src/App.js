c06ce07f85cdeca16a3715dbc7e58de2c29a6f86
waiyan <wphyo8929@gmail.com>
2023-02-08 15:50:32 +0630

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 
 
@@ -21,11 +22,12 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+      <ImageSearch />
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image} />
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
